cd8497ec08127074c958a869b3eb2fe8e7af8816
Stefano J. Attardi <stefano@storehouse.co>
2014-07-12 22:14:36 -0700

    Simpler usage, support for external require and transforms, remove examples

--- index.js
+++ index.js
@@ -1,65 +1,67 @@
-var gutil = require('gulp-util')
-var merge = require('deepmerge')
-var through = require('through2')
-var watchify = require('watchify')
+var path = require('path');
+var gutil = require('gulp-util');
+var merge = require('deepmerge');
+var through = require('through2');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
 
-var cache = {}
+var cache = {};
 
-module.exports = function(taskCallback) {
+function getBundle(file, opt) {
+  if (cache[file.path]) return cache[file.path];
 
-    function getBundle(file, opt) {
-        var path = file.path
-        if (cache[path]) {
-            return cache[path]
-        }
-        var bundle
-        if (opt.watch !== false) {
-            bundle = watchify(opt)
-            cache[path] = bundle
-            bundle.on('update', function() {
-                bundle.updateStatus = 'updated'
-                taskCallback(plugin)
-            })
-        } else {
-            bundle = watchify.browserify(opt)
-        }
-        bundle.updateStatus = 'first'
-        if (opt.setup) {
-            opt.setup(bundle)
-        }
+  var bundle = opt.watch ? watchify(opt) : watchify.browserify(opt);
+  cache[file.path] = bundle;
+  opt.setup && opt.setup(bundle);
 
-        return bundle
+  var self = './' + path.basename(file.relative, path.extname(file.relative));
+  opt.requireSelf && bundle.require(self);
+
+  var transforms = opt.transforms || [];
+  for (var i = 0; i < transforms.length; i++) {
+    bundle.transform(transforms[i]);
+  }
+
+  bundle.first = true;
+
+  return bundle;
+}
+
+module.exports = function(opt) {
+  return through.obj(function(file, enc, callback) {
+    if (file.isNull()) {
+      this.push(file); // Do nothing if no contents
+      return callback();
     }
-    function plugin(opt) {
-        return through.obj(function(file, enc, callback){
-            if (file.isNull()) {
-                this.push(file) // Do nothing if no contents
-                return callback()
-            }
-            if (file.isStream()) {
-                return callback(new Error('gulp-watchify ignores streams'))
-            }
-            var options = merge(opt, { entries:'./'+file.relative, basedir:file.base })
-            var bundle = getBundle(file, options)
-            if (bundle.updateStatus) {
-                gutil.log(
-                    bundle.updateStatus === 'first' ? "Bundling" : "Rebundling",
-                    gutil.colors.magenta(file.relative),
-                    opt.watch !== false ? '(watch mode)':''
-                )
-                file = file.clone()
-                delete bundle.updateStatus
-                file.contents = bundle.bundle(opt)
-                // Wait until done or else streamify(uglify()) fails due to buffering
-                file.contents.on('end', callback)
-                this.push(file)
-            } else {
-                callback()
-            }
-        })
+    if (file.isStream()) {
+      return callback(new Error('Streams are not supported'));
     }
-    // Return wrapped Task
-    return function() {
-        return taskCallback(plugin)
+    var options = merge(opt, {entries: './'+file.relative, basedir: file.base });
+
+    var stream = this;
+    var bundle = getBundle(file, options);
+
+    function rebundle() {
+      gutil.log(
+        bundle.first ? "Bundling" : "Rebundling",
+        gutil.colors.magenta(file.relative),
+        opt.watch !== false ? '(watch mode)':''
+      );
+
+      var newStream = bundle.bundle(opt).pipe(source('./'+file.relative));
+
+      newStream.on('data', function(data) {
+        stream.push(data);
+      });
+      newStream.on('end', function() {
+        bundle.first && callback();
+        if (opt.watch) bundle.first = false;
+      });
     }
+
+    bundle.on('update', rebundle);
+    rebundle();
+  }, function() {
+    if (!opt.watch) this.emit('end');
+  });
 }
