22d6f8c7dd50cb5c8f0a09cc11236d1da5bc13cb
Stefano J. Attardi <stefano@storehouse.co>
2014-07-23 20:41:44 -0700

    Add external and require options

--- index.js
+++ index.js
@@ -16,12 +16,24 @@ function getBundle(file, opt) {
   cache[file.path] = bundle;
   opt.setup && opt.setup(bundle);
 
-  var self = './' + path.basename(file.relative, path.extname(file.relative));
-  opt.requireSelf && bundle.require(self);
+  if (!(opt.require && opt.require.length)) {
+    var self = path.dirname(path.relative(opt.basedir || file.base, file.path)) + '/' + path.basename(file.path, path.extname(file.path));
+    opt.requireSelf && bundle.require(self);
+  }
 
   var transforms = opt.transforms || [];
   for (var i = 0; i < transforms.length; i++) {
     bundle.transform(transforms[i]);
+  }
+
+  var external = opt.external || [];
+  for (i = 0; i < external.length; i++) {
+    bundle.external(external[i][0], external[i][1]);
+  }
+
+  var require = opt.require || [];
+  for (i = 0; i < require.length; i++) {
+    bundle.require(require[i][0], require[i][1]);
   }
 
   bundle.first = true;
@@ -38,7 +50,8 @@ module.exports = function(opt) {
     if (file.isStream()) {
       return callback(new Error('Streams are not supported'));
     }
-    var options = merge(opt, {entries: './'+file.relative, basedir: file.base });
+    var base = opt.basedir || file.base;
+    var options = merge(opt, {entries: './' + path.relative(base, file.path), basedir: base });
 
     var stream = this;
     var bundle = getBundle(file, options);
